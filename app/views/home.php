bbeb0918077606fee09dd8c5781d290028acf5ee
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 12:20:19 -0600

    Actualizar home.php

--- app/views/home.php
+++ app/views/home.php
@@ -1,10 +1,6 @@
-<main>
-    <h1>Bienvenido a EscentialCore</h1>
-    <p>Este es un entorno básico para desarrollar sitios con PHP.</p>
-
-    <div class="three-divs">
-        <div class="box">Sección 1</div>
-        <div class="box">Sección 2</div>
-        <div class="box">Sección 3</div>
-    </div>
-</main>
+<!-- views/home.php -->
+<div class="container">
+    <div class="box">Div 1</div>
+    <div class="box">Div 2</div>
+    <div class="box">Div 3</div>
+</div>
