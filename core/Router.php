ce2ebe8049524d3749799c789611488c4acc60f2
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 01:52:41 -0600

    Actualizar Router.php

--- core/Router.php
+++ core/Router.php
@@ -6,8 +6,8 @@ class Router {
         $url = $_GET['url'] ?? 'home';
 
         // Convertir la URL en nombre de controlador
-        $controllerName = ucfirst(strtolower($url)) . "Controller";
-        $controllerClass = "App\\Controllers\\" . $controllerName;
+        $controllerName = ucfirst(strtolower($url)) . "controller";
+        $controllerClass = "app\\controllers\\" . $controllerName;
 
         // Verificar si el controlador existe
         if (class_exists($controllerClass)) {
