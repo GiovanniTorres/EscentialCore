b3335ab5cda9968fef39d330cae967e5e6ea391e
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 00:04:48 -0600

    Actualizar index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 // Incluir el enrutador y la configuración
 require_once __DIR__ . '/../core/Router.php';  // Incluye el enrutador
 require_once __DIR__ . '/../core/Config.php';   // Si tienes configuraciones generales
