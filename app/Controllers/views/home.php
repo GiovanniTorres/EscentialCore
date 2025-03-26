1b3bd7d6529c69accf08644b42ac6679bd3c5326
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 08:15:08 -0600

    Crear home.php

---
+++ app/Controllers/views/home.php
@@ -0,0 +1,10 @@
+<main>
+    <h1>Bienvenido a EscentialCore</h1>
+    <p>Este es un entorno básico para desarrollar sitios con PHP.</p>
+
+    <div class="three-divs">
+        <div class="box">Sección 1</div>
+        <div class="box">Sección 2</div>
+        <div class="box">Sección 3</div>
+    </div>
+</main>
