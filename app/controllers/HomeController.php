328f248b965bf83c5d29f20b930432f23b3db903
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 06:27:21 +0000

    index

--- app/controllers/HomeController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Controllers;
-
-class HomeController {
-    public function index() {
-        // Cargar la vista principal
-        require_once "../app/views/home.php";
-    }
-}
--- public/index.php
+++ public/index.php
@@ -1,14 +1,5 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-// Incluir el enrutador y la configuración
-require_once __DIR__ . '/../core/Router.php';  // Incluye el enrutador
-require_once __DIR__ . '/../core/Config.php';   // Si tienes configuraciones generales
 
-use Core\Router;
-
-// Crear una instancia del enrutador
-$router = new Router();
-
-// Manejar la solicitud
-$router->handleRequest();
+echo "El servidor está funcionando correctamente.";
