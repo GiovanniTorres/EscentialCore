f31425ccd6ab7f129a620d7b5af8703a0b565276
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 01:46:26 +0000

    core

---
+++ core/Router.php
@@ -0,0 +1,16 @@
+<?php
+namespace Core;
+
+class Router {
+    public function run() {
+            $route = $_GET['route'] ?? 'home';  // Ruta predeterminada
+                    $controller = 'App\\Controllers\\' . ucfirst($route) . 'Controller';
+
+                            if (class_exists($controller)) {
+                                        $instance = new $controller();
+                                                    $instance->index();
+                                                            } else {
+                                                                        echo "Página no encontrada";
+                                                                                }
+                                                                                    }
+                                                                                    }
