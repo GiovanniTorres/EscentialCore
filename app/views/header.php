2e9fe72a2b20b671293c03f4fa63d97e1035f4fc
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 13:18:43 -0600

    Actualizar header.php

--- app/views/header.php
+++ app/views/header.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EscentialCore</title>
-<link rel="stylesheet" href="styles.css">
-<link rel="stylesheet" href="divs.css">
 <link rel="stylesheet" href="menu.css">
+<link rel="stylesheet" href="divs.css">
 <link rel="stylesheet" href="footer.css">
+<link rel="stylesheet" href="style.css">
  <!-- Enlace a estilos -->
 </head>
 <body>
