85ab9b184b9c894f14db779cb785fd510c52f07a
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 14:14:01 -0600

    Actualizar menu.js

--- public/menu.js
+++ public/menu.js
@@ -1,16 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector(".nav-menu");
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
 
-    // Animación con clase 'open'
     menuToggle.addEventListener("click", function () {
-        navMenu.classList.toggle("open");
-    });
-
-    // Cierra el menú si se hace clic fuera de él
-    document.addEventListener("click", function (event) {
-        if (!menuToggle.contains(event.target) && !navMenu.contains(event.target)) {
-            navMenu.classList.remove("open");
-        }
+        menu.classList.toggle("active");
+        menuToggle.classList.toggle("active");
     });
 });
