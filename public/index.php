bdb132e39156de2e0cfb158bf22a24d5dc9a49b0
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 00:51:10 -0600

    Actualizar index.php

--- public/index.php
+++ public/index.php
@@ -3,5 +3,5 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Capturar la URL solicitada
-$url = $_GET['url'] ?? 'home';
+$url = $_GET['url'] ?? 'chango';
 echo "Has accedido a: " . htmlspecialchars($url);
