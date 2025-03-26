21ad52ac10b939df3bf0ed7dc6cfd70b532cc04d
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 01:50:27 +0000

    app

---
+++ app/HomeController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Controllers;
+
+class HomeController {
+    public function index() {
+            include '../app/views/header.php';
+                    include '../app/views/home.php';
+                            include '../app/views/footer.php';
+                                }
+                                }
