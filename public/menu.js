bb900c32ba4823507d2e310c6f20a074547ea12b
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 14:34:20 -0600

    Actualizar menu.js

--- public/menu.js
+++ public/menu.js
@@ -2,8 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.getElementById("menu-toggle");
     const menu = document.getElementById("menu");
 
-    menuToggle.addEventListener("click", function () {
+    // Función para alternar el menú
+    menuToggle.addEventListener("click", function (event) {
         menu.classList.toggle("active");
         menuToggle.classList.toggle("active");
+        event.stopPropagation(); // Evita que el clic se propague al document
+    });
+
+    // Detectar clic fuera del menú y cerrarlo
+    document.addEventListener("click", function (event) {
+        if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+            menu.classList.remove("active");
+            menuToggle.classList.remove("active");
+        }
     });
 });
