74485afbbc74aa85d61799216390a1287a8a2e70
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 16:44:38 -0600

    Actualizar header.php

--- app/views/header.php
+++ app/views/header.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi Sitio Web</title>
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
     <link rel="stylesheet" href="menu.css"> <!-- Menú Estilos -->
     <link rel="stylesheet" href="style.css"> <!-- Estilos Generales -->
    <link rel="stylesheet" href="home.css"> <!-- Estilos Generales -->
