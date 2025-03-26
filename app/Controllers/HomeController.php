4bf04cc04c87f54bd77c8ed88734706b29f013c1
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 08:18:08 -0600

    Actualizar HomeController.php

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,9 +1,19 @@
 <?php
+
 namespace App\Controllers;
 
-class HomeController {
-    public function index() {
-        echo "Bienvenido a la página de inicio.";
+class HomeController
+{
+    public function index()
+    {
+        // Llamamos a la función que carga la vista
+        $this->view('home');
+    }
+
+    private function view($view)
+    {
+        require_once '../app/views/header.php';
+        require_once "../app/views/{$view}.php";
+        require_once '../app/views/footer.php';
     }
 }
-//
