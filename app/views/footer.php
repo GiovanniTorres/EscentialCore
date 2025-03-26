13b5474c9ef1f5dc7d22832aefe35b967e0aae72
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 11:47:28 -0600

    Actualizar footer.php

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,5 +1,7 @@
 <footer>
     <p>&copy; <?php echo date("Y"); ?> EscentialCore - Todos los derechos reservados.</p>
 </footer>
+
+<script src="/menu.js"></script> <!-- Carga el script del menú -->
 </body>
 </html>
