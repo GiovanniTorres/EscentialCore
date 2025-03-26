ff1ec30e684b27ef4c71415c8f9201eebb124d8c
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 01:07:44 -0600

    Actualizar Router.php

--- core/Router.php
+++ core/Router.php
@@ -3,7 +3,7 @@ namespace Core;
 
 class Router {
     public function handleRequest() {
-            $url = $_GET['url'] ?? 'home';
+            $url = $_GET['url'] ?? 'panocha';
                     echo "Router: Has accedido a " . htmlspecialchars($url);
                         }
                         }
