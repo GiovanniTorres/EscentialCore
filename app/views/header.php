2447e3fccd7f4331925a0b926fe46c7c7bc17e54
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 14:49:20 -0600

    Actualizar header.php

--- app/views/header.php
+++ app/views/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi Sitio Web</title>
     <link rel="stylesheet" href="menu.css"> <!-- Menú Estilos -->
-    <link rel="stylesheet" href="style.css"> <!-- Estilos Generales -->
+    <link rel="stylesheet" href="home.css"> <!-- Estilos Generales -->
 </head>
 <body>
     <header>
