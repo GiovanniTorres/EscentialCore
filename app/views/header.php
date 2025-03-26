022c7ab6a608c0059c2d83382baea6038a1f9596
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-26 16:29:50 -0600

    Actualizar header.php

--- app/views/header.php
+++ app/views/header.php
@@ -5,12 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi Sitio Web</title>
-<script src="js/menu.js"></script>
+
     <link rel="stylesheet" href="menu.css"> <!-- Menú Estilos -->
     <link rel="stylesheet" href="style.css"> <!-- Estilos Generales -->
    <link rel="stylesheet" href="home.css"> <!-- Estilos Generales -->
 
    <link rel="stylesheet" href="footer.css"> <!-- Estilos Generales -->
+
+<script src="js/menu.js"></script>
 
 </head>
 <body>
