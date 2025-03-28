24ee0098d6d70bfb77e4be63717849985a55e265
Giovanni Torres <45153268+GiovanniTorres@users.noreply.github.com>
2025-03-27 19:38:59 -0600

    Actualizar header.php

--- app/views/header.php
+++ app/views/header.php
@@ -21,7 +21,7 @@
     <header>
         <div class="header-container">
             <div class="logo">
-                <a href="#">redLigths<span class="parentesis">()</span></a>
+                <a href="#">redLigths<span id="parentesis">()</span></a>
             </div>
             <nav class="navbar">
                 <ul class="menu" id="menu">
